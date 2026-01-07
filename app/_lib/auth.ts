2354d7d888f7c0e94ef1f360cb2492a1d58443ea
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-01-07 18:51:08 -0300

    feat: add correção de login

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -2,10 +2,16 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import { AuthOptions } from "next-auth"
 import { db } from "./prisma"
 import { Adapter } from "next-auth/adapters"
+import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
   callbacks: {
     async session({ session, user }) {
       session.user = {
@@ -13,8 +19,9 @@ export const authOptions: AuthOptions = {
         id: user.id,
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } as any
+
       return session
     },
   },
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 }
