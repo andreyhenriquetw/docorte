69bd4bc66c1f0515c5e38f4c80a3fd6113a7745c
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-01-11 15:52:56 -0300

    feat: add feat logo link para whastap

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,30 @@ import AuthProvider from "./_providers/auth"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "KN Do Corte", // Altere aqui para o nome da sua barbearia
+  title: "KN Do Corte",
   description:
     "Agendamentos rápidos na Barbearia KN Do Corte – estilo em primeiro lugar!",
-  icons: "/favicon.png",
+  icons: "/logkn.png",
+  openGraph: {
+    title: "KN Do Corte",
+    description: "Agende seu corte com estilo!",
+    url: "https://kndocorte.shop", // coloque seu domínio aqui
+    siteName: "KN Do Corte",
+    type: "website",
+    images: [
+      {
+        url: "/logkn.png", // imagem que aparecerá no WhatsApp
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "KN Do Corte",
+    description: "Agende seu corte com estilo!",
+    images: ["/logkn.png"],
+  },
 }
 
 export default function RootLayout({
