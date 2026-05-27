3269b28d19840a148944cf3ac8f4c75d66199183
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-05-27 20:14:34 -0300

    feat: add atualização dfsdfasd

--- app/api/bookings/upcoming/route.ts
+++ app/api/bookings/upcoming/route.ts
@@ -3,22 +3,7 @@ import { db } from "@/app/_lib/prisma"
 
 export async function GET() {
   try {
-    const now = new Date()
-
-    const thirtyMinutesLater = new Date(now.getTime() + 60 * 60 * 1000)
-
     const bookings = await db.booking.findMany({
-      where: {
-        reminderSent: false,
-
-        status: "CONFIRMED",
-
-        date: {
-          gte: now,
-          lte: thirtyMinutesLater,
-        },
-      },
-
       include: {
         user: true,
 
@@ -32,15 +17,7 @@ export async function GET() {
       },
     })
 
-    const filteredBookings = bookings.filter((booking) => {
-      const diff = booking.date.getTime() - now.getTime()
-
-      const minutes = diff / 1000 / 60
-
-      return minutes >= 29 && minutes <= 30
-    })
-
-    return NextResponse.json(filteredBookings)
+    return NextResponse.json(bookings)
   } catch (error) {
     console.error(error)
 
