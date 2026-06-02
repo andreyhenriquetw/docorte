006e4056942afd5ae305d38a076a9db1a7d76acc
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-06-02 18:59:54 -0300

    feat: add atualição via gpsadajsdhuajsd

--- app/api/reminders/route.ts
+++ app/api/reminders/route.ts
@@ -2,42 +2,19 @@ import { NextResponse } from "next/server"
 import { db } from "@/app/_lib/prisma"
 
 export async function GET() {
-  try {
-    const now = new Date()
+  const bookings = await db.booking.findMany({
+    where: {
+      reminderSent: false,
+    },
+  })
 
-    const bookings = await db.booking.findMany({
-      where: {
-        status: "CONFIRMED",
-        reminderSent: false,
-      },
-
-      include: {
-        user: true,
-        service: true,
-        barber: true,
-      },
-    })
-
-    const result = bookings.map((booking) => {
-      const diff = booking.date.getTime() - now.getTime()
-
-      const minutes = diff / 1000 / 60
-
-      return {
-        id: booking.id,
-        bookingDate: booking.date,
-        now,
-        diffMinutes: minutes,
-      }
-    })
-
-    return NextResponse.json(result)
-  } catch (error) {
-    console.error(error)
-
-    return NextResponse.json(
-      { error: "Erro ao buscar lembretes" },
-      { status: 500 },
-    )
-  }
+  return NextResponse.json(
+    bookings.map((booking) => ({
+      id: booking.id,
+      bookingDate: booking.date,
+      localHour: booking.date.toLocaleString("pt-BR", {
+        timeZone: "America/Sao_Paulo",
+      }),
+    })),
+  )
 }
