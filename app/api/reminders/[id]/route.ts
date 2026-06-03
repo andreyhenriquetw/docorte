8664ec9fe391639171e6ec75307554abcfb32c1e
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-06-03 19:31:19 -0300

    feat: dhucdshcidzr

--- app/api/reminders/[id]/route.ts
+++ app/api/reminders/[id]/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
+
 import { db } from "@/app/_lib/prisma"
 
 export async function PATCH(
@@ -6,8 +7,6 @@ export async function PATCH(
   { params }: { params: { id: string } },
 ) {
   try {
-    console.log("ID RECEBIDO:", params.id)
-
     const booking = await db.booking.update({
       where: {
         id: params.id,
@@ -19,17 +18,11 @@ export async function PATCH(
 
     return NextResponse.json(booking)
   } catch (error) {
-    console.error("ERRO PATCH:")
     console.error(error)
 
     return NextResponse.json(
-      {
-        error: "Erro ao atualizar lembrete",
-        details: String(error),
-      },
-      {
-        status: 500,
-      },
+      { error: "Erro ao atualizar lembrete" },
+      { status: 500 },
     )
   }
 }
