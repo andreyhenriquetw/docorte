6c615a4a587a5c220408f1869a8955ec85a2abd9
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-06-03 16:03:17 -0300

    feat: sadsa523as

--- app/api/reminders/route.ts
+++ app/api/reminders/route.ts
@@ -2,23 +2,12 @@ import { NextResponse } from "next/server"
 import { db } from "@/app/_lib/prisma"
 
 export async function GET() {
-  const now = new Date()
-
   const bookings = await db.booking.findMany({
-    where: {
-      status: "CONFIRMED",
-      reminderSent: false,
+    orderBy: {
+      createdAt: "desc",
     },
+    take: 5,
   })
 
-  return NextResponse.json({
-    serverTimeUTC: now.toISOString(),
-
-    bookings: bookings.map((booking) => ({
-      id: booking.id,
-      bookingDateUTC: booking.date.toISOString(),
-
-      diffMinutes: (booking.date.getTime() - now.getTime()) / 1000 / 60,
-    })),
-  })
+  return NextResponse.json(bookings)
 }
