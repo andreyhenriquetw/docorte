dd0be9303cb0bd2ea6a17ebd4434e941e16fcb88
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-06-03 15:51:30 -0300

    feat: asdhuisadaiughv cda

--- app/api/reminders/route.ts
+++ app/api/reminders/route.ts
@@ -1,23 +1,52 @@
 import { NextResponse } from "next/server"
 import { db } from "@/app/_lib/prisma"
+import { toZonedTime } from "date-fns-tz"
 
 export async function GET() {
-  const now = new Date()
+  try {
+    const now = toZonedTime(new Date(), "America/Sao_Paulo")
 
-  const bookings = await db.booking.findMany({
-    where: {
-      status: "CONFIRMED",
-      reminderSent: false,
-    },
-  })
+    const bookings = await db.booking.findMany({
+      where: {
+        status: "CONFIRMED",
+        reminderSent: false,
+      },
+    })
 
-  return NextResponse.json({
-    serverNow: now.toISOString(),
-    serverNowLocal: now.toString(),
-    bookings: bookings.map((booking) => ({
-      id: booking.id,
-      bookingDate: booking.date.toISOString(),
-      diffMinutes: (booking.date.getTime() - now.getTime()) / 1000 / 60,
-    })),
-  })
+    const reminders = bookings.filter((booking) => {
+      const bookingDate = toZonedTime(booking.date, "America/Sao_Paulo")
+
+      const minutes = (bookingDate.getTime() - now.getTime()) / 1000 / 60
+
+      console.log({
+        now: now.toString(),
+        booking: bookingDate.toString(),
+        minutes,
+      })
+
+      return NextResponse.json({
+        serverNowUTC: new Date().toISOString(),
+
+        serverNowBrazil: toZonedTime(
+          new Date(),
+          "America/Sao_Paulo",
+        ).toString(),
+
+        bookingDate: bookings[0]?.date,
+      })
+    })
+
+    return NextResponse.json(reminders)
+  } catch (error) {
+    console.error(error)
+
+    return NextResponse.json(
+      {
+        error: "Erro",
+      },
+      {
+        status: 500,
+      },
+    )
+  }
 }
