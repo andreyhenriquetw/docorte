7f2ca986cad6217d1fb26df1d6b5cd8606b88f70
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-06-03 14:58:24 -0300

    feat: add atualiçao do horario em route

--- app/api/reminders/route.ts
+++ app/api/reminders/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server"
 import { db } from "@/app/_lib/prisma"
+import { toZonedTime } from "date-fns-tz"
 
 export async function GET() {
   try {
-    const now = new Date()
+    const now = toZonedTime(new Date(), "America/Sao_Paulo")
 
     const bookings = await db.booking.findMany({
       where: {
@@ -19,7 +20,16 @@ export async function GET() {
     })
 
     const reminders = bookings.filter((booking) => {
-      const minutes = (booking.date.getTime() - now.getTime()) / 1000 / 60
+      const bookingDate = toZonedTime(booking.date, "America/Sao_Paulo")
+
+      const minutes = (bookingDate.getTime() - now.getTime()) / 1000 / 60
+
+      console.log({
+        cliente: booking.user.name,
+        agora: now.toISOString(),
+        agendamento: bookingDate.toISOString(),
+        faltam: minutes,
+      })
 
       return minutes >= 28 && minutes <= 30
     })
