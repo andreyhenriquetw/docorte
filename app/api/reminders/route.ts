0b9379f2ad71eb76e45ac90beff71bc4c6bb9cb6
andreyhenriquetw <andreyhenriquetw2@gmail.com>
2026-06-05 19:25:37 -0300

    feat: asd32qads

--- app/api/reminders/route.ts
+++ app/api/reminders/route.ts
@@ -28,7 +28,7 @@ export async function GET() {
     const reminders = bookings.filter((booking) => {
       const diffMinutes = (booking.date.getTime() - now.getTime()) / 1000 / 60
 
-      return diffMinutes <= 300
+      return diffMinutes >= 28 && diffMinutes <= 31
     })
 
     return NextResponse.json(reminders)
